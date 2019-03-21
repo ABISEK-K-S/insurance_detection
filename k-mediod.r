394b6d0ee900b7d43a19ee83b33c7a1ea7a7a43e
ABISEK-K-S <39756233+ABISEK-K-S@users.noreply.github.com>
2019-03-22 00:42:17 +0530

    Create k-mediod.r

---
+++ k-mediod.r
@@ -0,0 +1,20 @@
+
+ rm(list=ls())
+ dataset0=read.csv("test.csv",header=T)
+ dataset=dataset0[1:5]
+ 
+ 
+ df=dataset
+ print(df)
+ df=dataset[4:5]
+ print(df) 
+ head(df, n = 3)
+ pam(x, k, metric = "euclidean", stand = FALSE)
+
+ pam.res <- pam(df, 3)
+ print(pam.res)
+ dd <- cbind(df, cluster = pam.res$cluster)
+ head(dd, n = 3)
+ pam.res$medoids
+ head(pam.res$clustering)
+ fviz_cluster(pam.res, geom = "point", ellipse.type = "norm")
