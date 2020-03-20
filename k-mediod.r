697b24afcbd0aaba1bd42ceb67c8259b1abf6193
ABISEK-K-S <39756233+ABISEK-K-S@users.noreply.github.com>
2020-03-20 11:18:26 +0530

    Update k-mediod.r

--- k-mediod.r
+++ k-mediod.r
@@ -1,24 +1,23 @@
 
- rm(list=ls())
- dataset0=read.csv("test.csv",header=T)
- dataset=dataset0[1:5]
+rm(list=ls())
+setwd("C:/Users/ABISEK/Downloads/kk")
+dataset0=read.csv("test.csv",header=T)
+dataset=dataset0[1:5]
+library("factoextra")
+library(cluster)
+df=dataset
+print(df)
+df=dataset[4:5]
+print(df) 
+head(df, n = 3)
+pam(x, k, metric = "euclidean", stand = FALSE)
+
+pam.res <- pam(df, 3)
+print(pam.res)
+dd <- cbind(df, cluster = pam.res$cluster)
+head(dd, n = 3)
+pam.res$medoids
+head(pam.res$clustering)
+fviz_cluster(pam.res, geom = "point", ellipse.type = "norm")
 
 
- library("factoextra")
- library(cluster)
-
-
- df=dataset
- print(df)
- df=dataset[4:5]
- print(df) 
- head(df, n = 3)
- pam(x, k, metric = "euclidean", stand = FALSE)
-
- pam.res <- pam(df, 3)
- print(pam.res)
- dd <- cbind(df, cluster = pam.res$cluster)
- head(dd, n = 3)
- pam.res$medoids
- head(pam.res$clustering)
- fviz_cluster(pam.res, geom = "point", ellipse.type = "norm")
